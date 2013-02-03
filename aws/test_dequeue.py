404ba28e1b8019bff8a392bdc75425a067860489
Ubuntu <ubuntu@ip-10-98-58-88.ec2.internal>
2013-02-03 08:23:47 +0000

    Don't really know, want to pull and found local changes.

--- aws/test_dequeue.py
+++ aws/test_dequeue.py
@@ -10,14 +10,14 @@ dest_dir = sys.stdin.readline().strip()
 e_delay = 0.1
 
 while True:
-    print '  > waiting to process a frame'
+    #print '  > waiting to process a frame'
     succ = dequeue_frame(dest_dir)
     if succ:
         print '  > success!'
         if e_delay > 0.001:
             e_delay = e_delay / 2.0
     else:
-        print '  > queue empty! backing off for ',
+        #print '  > queue empty! backing off for ',
         print str(e_delay) + ' seconds'
         time.sleep(e_delay)
         if e_delay < 60.0:
