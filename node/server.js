f580c779bb86bd98daaf1afe2ddb94eab067ba1d
Dan Roberts <droberts@purdue.edu>
2013-03-01 23:31:19 -0500

    More progress on node

--- node/server.js
+++ node/server.js
@@ -2,10 +2,13 @@
 var http = require("http");
 var url  = require("url");
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Handling request for " + pathname);
+
+        route(handle, pathname);
+
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello, World, from a notorious function");
         response.end();
