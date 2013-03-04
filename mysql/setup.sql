f023d7b85d4103064c906a3399d4fbe1c7c63105
Christopher Cole <cole14@purdue.edu>
2013-03-04 16:54:21 +0000

    Fix the username credentials in the db.

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -11,9 +11,9 @@
 CREATE DATABASE irisdb;
 
 -- This user will be run from the python, with write privelages
-CREATE USER 'iris_ocr'@'localhost' IDENTIFIED BY 'blueballoon';
-GRANT ALL ON irisdb.* TO 'iris_ocr'@'localhost';
+CREATE USER 'iris_ocr'@'domU-12-31-39-14-72-A7.compute-1.internal' IDENTIFIED BY 'blueballoon';
+GRANT ALL ON irisdb.* TO 'iris_ocr'@'domU-12-31-39-14-72-A7.compute-1.internal';
 
 -- This user is the webapp, and can only read.
-CREATE USER 'iris_webapp'@'localhost' IDENTIFIED BY 'redeagle';
-GRANT SELECT ON irisdb.* TO 'iris_webapp'@'localhost';
+CREATE USER 'iris_webapp'@'domU-12-31-39-14-72-A7.compute-1.internal' IDENTIFIED BY 'redeagle';
+GRANT SELECT ON irisdb.* TO 'iris_webapp'@'domU-12-31-39-14-72-A7.compute-1.internal';
